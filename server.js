963a1d7b01b735ff54cc986a011cb5e4b0c84569
13824125580 <13824125580@163.com>
2018-08-05 18:29:36 +0800

    2nd update

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var http = require("http");
 // 创建server，指定处理客户端请求的函数
 http.createServer(
     function(request, response) { 
+        console.log("method - " + request.method);
+        console.log("version - " + request.httpVersion);
+        console.log("url - " + request.url);
         response.writeHead(200, {"Content-Type": "text/plain"}); 
         response.write("Hello World!"); 
         response.end(); 
     }
 ).listen(8000); 
 
-console.log("Hello World is listening at port 8000");
+console.log("Hello World start listen on port 8000");
